3613dbe3760a658942116c7823b390fe8e27c188
Maksym Olhovych <firebirth@gmail.com>
2018-12-06 09:29:41 +0200

    wip

--- ProjectBulkProcessor.Main/OptionsParser.fs
+++ ProjectBulkProcessor.Main/OptionsParser.fs
@@ -4,39 +4,61 @@ open System.IO
 open System.Collections.Generic
 open Microsoft.CodeAnalysis.CSharp
 open Microsoft.CodeAnalysis.CSharp.Syntax
+open System.Xml.Linq
 
-type Options = {targetFramework:string; isExecutable:bool; copyright: string; company: string; authors: string; description: string; version: string; product: string }
+type Options = { targetFramework:string; isExecutable:bool; copyright: string; company: string; authors: string; description: string; version: string; product: string }
 
-let private oldNewFrameworkAliasMap = dict[
-                                            "v4.6.2", "net462";
-                                            "v4.6.1", "net461";
-                                          ]
-
-let private findAssemblyInfoFiles projectPath = Directory.GetFiles(projectPath, "AssemblyInfo.cs", SearchOption.AllDirectories)
+let private findAssemblyInfoFiles projectDir = Directory.GetFiles(projectDir, "AssemblyInfo.cs", SearchOption.AllDirectories)
 
 let private getAttributeList assemblyInfoFile = 
-        use fs = File.OpenText assemblyInfoFile
-        let tree = fs.ReadToEnd() |> CSharpSyntaxTree.ParseText
-        let cus = tree.GetRoot() :?> CompilationUnitSyntax
-        cus.AttributeLists
+    using (File.OpenText assemblyInfoFile) (fun fs -> fs.ReadToEnd())
+    |> CSharpSyntaxTree.ParseText
+    |> fun tree -> tree.GetRoot() :?> CompilationUnitSyntax
+    |> fun cus -> cus.AttributeLists
 
-let private buildAttributeOptions (attributeList: IEnumerable<AttributeListSyntax>) =
+let private buildAttributeOptions (attributeList: IEnumerable<AttributeListSyntax>) opts =
     let buildAttributeValue (attribute: AttributeSyntax) = attribute.ArgumentList.Arguments |> Seq.map (fun a -> a.ToFullString().Trim '"') |> String.concat ","
-    let mutable options = {targetFramework = ""; isExecutable= false; copyright= ""; company= ""; authors= ""; description= ""; version= ""; product= ""}
-    let allAttributes = attributeList |> Seq.collect (fun attributeList -> attributeList.Attributes)
-    for attribute in allAttributes do
+
+    let mutable options = opts
+    for attribute in attributeList |> Seq.collect (fun attributeList -> attributeList.Attributes) do
         match attribute.Name with
-        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyCompany" -> options <- {options with company = buildAttributeValue attribute }
-        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyCopyright" -> options <- {options with copyright = buildAttributeValue attribute }
-        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyDescription" -> options <- {options with description = buildAttributeValue attribute }
-        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyProduct" -> options <- {options with product = buildAttributeValue attribute }
-        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyVersion" -> options <- {options with version = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyCompany" -> options <- { options with company = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyCopyright" -> options <- { options with copyright = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyDescription" -> options <- { options with description = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyProduct" -> options <- { options with product = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyVersion" -> options <- { options with version = buildAttributeValue attribute }
         | _ -> ()
     options
 
-let private buildAssemblyInfoOptions projectPath =
-    projectPath
+let private buildAssemblyInfoOptions projectDir =
+    projectDir
     |> findAssemblyInfoFiles
     |> Seq.exactlyOne
     |> getAttributeList
     |> buildAttributeOptions
+
+let private readProjectFile projectDir =
+    Directory.GetFiles(projectDir, "*.csproj")
+    |> Seq.exactlyOne
+    |> fun projFilePath -> using (File.OpenRead projFilePath) XDocument.Load
+
+let private buildFrameworkOptions (xdoc: XDocument) opts =
+    let mutable options = opts
+    let oldFramework = XmlHelpers.getProjectElementByName xdoc "TargetFrameworkVersion"
+    let a = match oldFramework with
+        | null -> ()
+        | _ -> _
+    let newFramework = match oldFramework.Value with
+                       | "v4.6.2" -> "net462"
+                       | _ -> "net471"
+    options <- {options with targetFramework = newFramework}
+    options
+
+let private buildOutputTypeOptions (xdoc: XDocument) opts =
+    let outputTypeElement = XmlHelpers.getProjectElementByName xdoc "OutputType"
+    let outputType = match outputTypeElement.Value with
+        | 
+
+let private a =
+    readProjectFile ""
+    |> buildFrameworkOptions
---
+++ ProjectBulkProcessor.Main/XmlHelpers.fs
@@ -0,0 +1,15 @@
+﻿module XmlHelpers
+
+open System.Xml
+open System.Xml.Linq
+open System.Xml.XPath
+
+let private projectNamespaceManager = new XmlNamespaceManager(new NameTable())
+do
+    projectNamespaceManager.AddNamespace("project", "http://schemas.microsoft.com/developer/msbuild/2003")
+
+let getProjectElementByName (xElement:XNode) name =
+    xElement.XPathSelectElement("//project:" + name, projectNamespaceManager)
+
+let getProjectElementsByName (xElement:XNode) name =
+    xElement.XPathSelectElements("//project:" + name, projectNamespaceManager)
