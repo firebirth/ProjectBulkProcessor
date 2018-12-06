caa7db5998010014d8627248abd493ea9069d6ed
Maksym Olhovych <firebirth@gmail.com>
2018-12-06 08:27:47 +0200

    Added F# implementations for Scanner and partially for Parser

---
+++ ProjectBulkProcessor.Main/OptionsParser.fs
@@ -0,0 +1,42 @@
+﻿module Parser
+
+open System.IO
+open System.Collections.Generic
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis.CSharp.Syntax
+
+type Options = {targetFramework:string; isExecutable:bool; copyright: string; company: string; authors: string; description: string; version: string; product: string }
+
+let private oldNewFrameworkAliasMap = dict[
+                                            "v4.6.2", "net462";
+                                            "v4.6.1", "net461";
+                                          ]
+
+let private findAssemblyInfoFiles projectPath = Directory.GetFiles(projectPath, "AssemblyInfo.cs", SearchOption.AllDirectories)
+
+let private getAttributeList assemblyInfoFile = 
+        use fs = File.OpenText assemblyInfoFile
+        let tree = fs.ReadToEnd() |> CSharpSyntaxTree.ParseText
+        let cus = tree.GetRoot() :?> CompilationUnitSyntax
+        cus.AttributeLists
+
+let private buildAttributeOptions (attributeList: IEnumerable<AttributeListSyntax>) =
+    let buildAttributeValue (attribute: AttributeSyntax) = attribute.ArgumentList.Arguments |> Seq.map (fun a -> a.ToFullString().Trim '"') |> String.concat ","
+    let mutable options = {targetFramework = ""; isExecutable= false; copyright= ""; company= ""; authors= ""; description= ""; version= ""; product= ""}
+    let allAttributes = attributeList |> Seq.collect (fun attributeList -> attributeList.Attributes)
+    for attribute in allAttributes do
+        match attribute.Name with
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyCompany" -> options <- {options with company = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyCopyright" -> options <- {options with copyright = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyDescription" -> options <- {options with description = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyProduct" -> options <- {options with product = buildAttributeValue attribute }
+        | :? IdentifierNameSyntax as ins when ins.Identifier.Text = "AssemblyVersion" -> options <- {options with version = buildAttributeValue attribute }
+        | _ -> ()
+    options
+
+let private buildAssemblyInfoOptions projectPath =
+    projectPath
+    |> findAssemblyInfoFiles
+    |> Seq.exactlyOne
+    |> getAttributeList
+    |> buildAttributeOptions
---
+++ ProjectBulkProcessor.Main/Scanner.fs
@@ -0,0 +1,19 @@
+﻿module Scanner
+
+open System.IO
+
+type private pathValidationResult = { isValid: bool }
+
+let private validateDirectory rootPath = { isValid = Directory.Exists(rootPath)  }
+
+let private getFileList rootPath = Directory.GetFiles(rootPath, "*.csproj", SearchOption.AllDirectories)
+
+let private matchValidationResult validationResult = 
+    match validationResult with
+    | { isValid = true } -> getFileList
+    | _ -> fun _ -> Array.empty
+
+let getProjectFiles rootPath = 
+    rootPath
+    |> validateDirectory
+    |> matchValidationResult
